27cf0edf07a3f4ec922c62151ab7d1408351a003
Salwa <salwasanaullahkhan@gmail.com>
2019-11-09 15:32:16 +0800

    changed title of books

---
+++ books.js
@@ -0,0 +1,36 @@
+books:[
+            {
+                id: 1,
+                title: "Discovering Maths,1A",
+                author: "Alan",
+                price: "30",
+                level: "Secondary 1",
+                img:"discovering maths 1A.jpg",
+
+            },
+            {
+                id: 2,
+                title: "Discovering Maths,1B",
+                author: "Alan",
+                price: "30",
+                level: "Secondary 1",
+                img: "discovering maths 1B.jpg" ,
+            },
+            {
+                id: 3,
+                title: "Jauhari,2A",
+                author: "Rob",
+                price: "20",
+                level: "Secondary 2",
+                img: "jauhari 2A.jpg",
+                
+            },
+            {
+                id: 4,
+                title: "Jauhari,2B",
+                author: "Rob",
+                price: "20",
+                level: "Secondary 2",
+                img: "jauhari 2B.jpg" ,
+            }]
+
